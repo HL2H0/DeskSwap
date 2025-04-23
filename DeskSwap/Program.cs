7369784f38dcab0002b463dd00107997a589a190
HL2H0 <gmhisham7@gmail.com>
2025-04-23 13:33:37 +0300

    Add project files.

---
+++ DeskSwap/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace DeskSwap;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string DesktopPath = Environment.GetFolderPath(Environment.SpecialFolder.DesktopDirectory);
+        string DumpPath = Path.Combine(DesktopPath, "DeskSwapDump");
+
+        string[] files = Directory.GetFiles(DesktopPath, "*.*", SearchOption.TopDirectoryOnly);
+        string[] folders = Directory.GetDirectories(DesktopPath, "*", SearchOption.TopDirectoryOnly);
+        string[] shortcuts = Directory.GetFiles(DesktopPath, "*.lnk", SearchOption.TopDirectoryOnly);
+        string[] all = files.Concat(folders).Concat(shortcuts).ToArray();
+        
+        if (!Directory.Exists(DumpPath))
+        {
+            Directory.CreateDirectory(DumpPath);
+        }
+        foreach (string item in all)
+        {
+            var path = Path.Combine(DumpPath, Path.GetFileName(item));
+            if (Path.GetFileName(item) == "DeskSwapDump")
+            {
+                Console.WriteLine($"Skipping {item} as it is the dump folder itself.");
+                continue;
+            }
+            if (File.Exists(path) || Directory.Exists(path))
+            {
+                Console.WriteLine($"Skipping {item} as it already exists in the dump folder.");
+                continue;
+            }
+            if (File.Exists(item))
+            {
+                File.Move(item, path);
+            }
+            else if (Directory.Exists(item))
+            {
+                Directory.Move(item, path);
+            }
+            Console.WriteLine(item);
+        }
+        Console.ReadKey();
+    }
+}
